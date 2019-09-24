04a8c0fc66c776b5725ea6f9459315461ac8bcf9
Vijay Somers <vijay.somers@avidbots.com>
2019-09-24 18:34:00 -0400

    Remove old helloworld code and start video input

--- Progressive.cpp
+++ Progressive.cpp
@@ -5,24 +5,34 @@ using namespace cv;
 
 int main(int argc, char** argv )
 {
-    if ( argc != 2 )
-    {
-        printf("usage: DisplayImage.out <Image_Path>\n");
-        return -1;
-    }
+  VideoCapture inputvideo(0);
+  if (!inputvideo.isOpened()){
+    std::cout  << "Could not open reference " << 0 << std::endl;
+    return -1;
+  }
 
-    Mat image;
-    image = imread( argv[1], 1 );
+  std::string window_name = "This is a window";
 
-    if ( !image.data )
-    {
-        printf("No image data \n");
-        return -1;
-    }
-    namedWindow("Display Image", WINDOW_AUTOSIZE );
-    imshow("Display Image", image);
+  namedWindow(window_name.c_str(), WINDOW_AUTOSIZE);
 
-    waitKey(0);
+    // if ( argc != 2 )
+    // {
+    //     printf("usage: DisplayImage.out <Image_Path>\n");
+    //     return -1;
+    // }
 
-    return 0;
+    // Mat image;
+    // image = imread( argv[1], 1 );
+
+    // if ( !image.data )
+    // {
+    //     printf("No image data \n");
+    //     return -1;
+    // }
+    // namedWindow("Display Image", WINDOW_AUTOSIZE );
+    // imshow("Display Image", image);
+
+    // waitKey(0);
+
+    // return 0;
 }
