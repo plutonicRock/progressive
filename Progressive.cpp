041673d18e9face1f9644e538be033d431e39857
noone <alpha@notreal.com>
2019-09-27 00:18:04 -0400

    Add camera params, First attempt at undistorting camera

--- Progressive.cpp
+++ Progressive.cpp
@@ -3,6 +3,10 @@
 
 using namespace cv;
 
+void printsize(Mat matrix){
+
+};
+
 int main(int argc, char** argv )
 {
   VideoCapture inputvideo(0);
@@ -24,15 +28,45 @@ int main(int argc, char** argv )
 
   namedWindow(window_name.c_str(), WINDOW_AUTOSIZE);
   moveWindow(window_name.c_str(), 400, 0);
+
+  // Read in file data
+  std::string camera_param_file = "camParams.xml";
+  FileStorage fs(camera_param_file, FileStorage::READ);
+  // 
+  cv::Mat undistMap1, undistMap2;
+
+  cv::Size image_size;
+  Mat cameraMatrix = Mat_<double>::zeros(3,3);
+  Mat dist_coeffs = Mat_<double>::zeros(1,5);
+  fs << "cameraResolution" << image_size;
+  fs << "cameraMatrix" << cameraMatrix;
+  fs << "dist_coeffs" << dist_coeffs;
+
   
-  
+
+      // cv::initUndistortRectifyMap(mCalibData->cameraMatrix, mCalibData->distCoeffs, cv::noArray(),
+                                // cv::getOptimalNewCameraMatrix(mCalibData->cameraMatrix, mCalibData->distCoeffs, mCalibData->imageSize, 0.0, mCalibData->imageSize),
+                                // mCalibData->imageSize, CV_16SC2, mCalibData->undistMap1, mCalibData->undistMap2);
+
+  // cv::initUndistortRectifyMap(cameraMatrix, dist_coeffs, cv::noArray(), 
+  // cv::getOptimalNewCameraMatrix(cameraMatrix, dist_coeffs, image_size, 0.0, image_size), 
+  // image_size, CV_16SC2, undistMap1, undistMap2 );
+
+    cv::initUndistortRectifyMap(cameraMatrix, dist_coeffs, cv::noArray(), cameraMatrix, image_size, CV_16SC2, undistMap1, undistMap2 );
 
   for (;;){
     inputvideo >> oneFrame;
     ++ frameNum;
     
+    
     putText(oneFrame, "Testing text rendering", org, FONT_HERSHEY_SIMPLEX, 1.0, colorScalar);
-    imshow(window_name.c_str(), oneFrame);
+
+    // Undistort
+    cv::Mat oneFrame_noDistort;
+    std::cout<< oneFrame.size().height<< " " << oneFrame.size().width << std::endl;
+    cv::remap(oneFrame, oneFrame_noDistort, undistMap1, undistMap2, cv::INTER_LINEAR);
+    std::cout<< oneFrame_noDistort.size().height<< " " << oneFrame_noDistort.size().width << std::endl;
+    imshow(window_name.c_str(), oneFrame_noDistort);
 
     char c = (char)waitKey(delay);
     if ( c==27 ) break;
