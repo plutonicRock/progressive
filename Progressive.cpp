fcc1108600efd57802949b95c4846c721f95aefb
noone <alpha@notreal.com>
2019-09-29 22:08:07 -0400

    Undistortion works

--- Progressive.cpp
+++ Progressive.cpp
@@ -17,50 +17,6 @@ void PrintMatrix(std::string name, cv::Mat matrix){
   }
 };
 
- /// Global variables
- Mat src;
- Mat map_x, map_y;
-//  char* remap_window = "Remap demo";
- int ind = 0;
-
-void update_map( void )
-{
-  ind = ind%4;
-
-  for( int j = 0; j < src.rows; j++ )
-  { for( int i = 0; i < src.cols; i++ )
-      {
-        switch( ind )
-        {
-          case 0:
-            if( i > src.cols*0.25 && i < src.cols*0.75 && j > src.rows*0.25 && j < src.rows*0.75 )
-              {
-                map_x.at<float>(j,i) = 2*( i - src.cols*0.25 ) + 0.5 ;
-                map_y.at<float>(j,i) = 2*( j - src.rows*0.25 ) + 0.5 ;
-              }
-            else
-              { map_x.at<float>(j,i) = 0 ;
-                map_y.at<float>(j,i) = 0 ;
-              }
-                break;
-          case 1:
-                map_x.at<float>(j,i) = i ;
-                map_y.at<float>(j,i) = src.rows - j ;
-                break;
-          case 2:
-                map_x.at<float>(j,i) = src.cols - i ;
-                map_y.at<float>(j,i) = j ;
-                break;
-          case 3:
-                map_x.at<float>(j,i) = src.cols - i ;
-                map_y.at<float>(j,i) = src.rows - j ;
-                break;
-        } // end of switch
-      }
-  }
-ind++;
-};
-
 int main(int argc, char** argv )
 {
   VideoCapture inputvideo(0);
@@ -77,22 +33,20 @@ int main(int argc, char** argv )
   // Kind of greenish
   Scalar colorScalar = cv::Scalar( 94, 206, 165 );
   Mat oneFrame;
+
+  // Screen text data
   Point org;
   org.x = 0;
   org.y = 40;
+  std::string screen_text = "Testing text rendering";
 
-  inputvideo >> oneFrame;
-  map_x.create( oneFrame.size(), CV_32FC1 );
-  map_y.create( oneFrame.size(), CV_32FC1 );
-
-
-
+  // Create window
   namedWindow(window_name.c_str(), WINDOW_AUTOSIZE);
   moveWindow(window_name.c_str(), 400, 0);
 
   cv::Mat undistMap1, undistMap2;
-  undistMap1.create( oneFrame.size(), CV_16SC2 );
-  undistMap2.create( oneFrame.size(), CV_16SC2 );
+  // undistMap1.create( oneFrame.size(), CV_16SC2 );
+  // undistMap2.create( oneFrame.size(), CV_16SC2 );
 
   // Read in file data
   std::string camera_param_file = "../camParams.xml";
@@ -104,57 +58,25 @@ int main(int argc, char** argv )
   cv::Size image_size;
   Mat cameraMatrix = Mat_<double>::zeros(3,3);
   Mat dist_coeffs = Mat_<double>::zeros(1,5);
-  fs["cameraResolution"] >> image_size;
-  cv::Size small_image_size(1920,1080);
   
-  // small_image_size.height(480);
-  // small_image_size.width(640);
+  fs["cameraResolution"] >> image_size;  
   fs["cameraMatrix"] >> cameraMatrix;
-  // fs << "cameraMatrix" << cameraMatrix;
-  PrintMatrix("cameraMatrix", cameraMatrix);
-  fs["dist_coeffs"] >> dist_coeffs;
-  PrintMatrix("dist_coeffs", dist_coeffs);
-  
+  fs["dist_coeffs"] >> dist_coeffs;  
 
-  return 0;
-  
+  // Generate undistortion maps
+  cv::initUndistortRectifyMap(cameraMatrix, dist_coeffs, cv::noArray(), cameraMatrix, image_size, CV_16SC2, undistMap1, undistMap2 );
 
-  // std::cout<<dist_coeffs[0]<<std::endl;
-
-  
-
-      // cv::initUndistortRectifyMap(mCalibData->cameraMatrix, mCalibData->distCoeffs, cv::noArray(),
-                                // cv::getOptimalNewCameraMatrix(mCalibData->cameraMatrix, mCalibData->distCoeffs, mCalibData->imageSize, 0.0, mCalibData->imageSize),
-                                // mCalibData->imageSize, CV_16SC2, mCalibData->undistMap1, mCalibData->undistMap2);
-
-  // cv::initUndistortRectifyMap(cameraMatrix, dist_coeffs, cv::noArray(), 
-  // cv::getOptimalNewCameraMatrix(cameraMatrix, dist_coeffs, image_size, 0.0, image_size), 
-  // image_size, CV_16SC2, undistMap1, undistMap2 );
-
-    cv::initUndistortRectifyMap(cameraMatrix, dist_coeffs, cv::noArray(), cv::getOptimalNewCameraMatrix(cameraMatrix, dist_coeffs, small_image_size, 0.0, small_image_size), small_image_size, CV_16SC2, undistMap1, undistMap2 );
-//cv::getOptimalNewCameraMatrix(cameraMatrix, distCoeffs, small_image_size, 0.0, small_image_size)
   for (;;){
     inputvideo >> oneFrame;
     ++ frameNum;
-    // ((map1.type() == CV_32FC2 || map1.type() == CV_16SC2) && map2.empty()) 
-    // || 
-    // (map1.type() == CV_32FC1 && map2.type() == CV_32FC1)PrintMatrix("cameraMatrix", cameraMatrix);
-
     
-    putText(oneFrame, "Testing text rendering", org, FONT_HERSHEY_SIMPLEX, 1.0, colorScalar);
+    putText(oneFrame, screen_text, org, FONT_HERSHEY_SIMPLEX, 1.0, colorScalar);
 
     // Undistort
-    cv::Mat oneFrame_noDistort;
-    
-    src = oneFrame;
-    update_map();
+    cv::Mat oneFrame_noDistort;    
     cv::remap(oneFrame, oneFrame_noDistort, undistMap1, undistMap2, cv::INTER_LINEAR);
-    //cv::remap(oneFrame, oneFrame_noDistort, map_x, map_y, cv::INTER_LINEAR);//, cv::BORDER_CONSTANT, Scalar(0,0, 0) );
-    PrintMatrixSize("original frame", oneFrame);
-    PrintMatrixSize("undistort matrix 1", undistMap1);
-    PrintMatrixSize("undistort matrix 2", undistMap2);
-    PrintMatrixSize("NoDistort", oneFrame_noDistort);
-    // imshow(window_name.c_str(), oneFrame_noDistort);
+    
+    // Show image on screen
     imshow(window_name.c_str(), oneFrame_noDistort);
 
     char c = (char)waitKey(delay);
